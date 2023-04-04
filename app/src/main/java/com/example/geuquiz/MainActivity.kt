79602c7b2415646fd7e6bc8bacccafd64cfe77df
Max <maxschew@yandex.ru>
2023-04-04 14:06:11 +0200

    update MainActivity: add nextButton and changing questions on View

--- app/src/main/java/com/example/geuquiz/MainActivity.kt
+++ app/src/main/java/com/example/geuquiz/MainActivity.kt
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -28,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
             Toast.makeText(
@@ -45,5 +50,17 @@ class MainActivity : AppCompatActivity() {
             )
                 .show()
         }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
