5ec45b7a4286c8ab8666560e35a934f684cf1120
Max <maxschew@yandex.ru>
2023-04-04 13:55:26 +0200

    add data class Question, stringRes

--- app/src/main/java/com/example/geuquiz/MainActivity.kt
+++ app/src/main/java/com/example/geuquiz/MainActivity.kt
@@ -11,6 +11,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_ocean, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,14 +33,16 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(
                 this,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT)
+                Toast.LENGTH_SHORT
+            )
                 .show()
         }
         falseButton.setOnClickListener {
             Toast.makeText(
                 this,
                 R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
+                Toast.LENGTH_SHORT
+            )
                 .show()
         }
     }
